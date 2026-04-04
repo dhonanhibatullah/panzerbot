25bf738d0eb1b61d28987cd6812b74f507afb51b
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2026-04-04 14:28:38 +0700

    refactor: infinite loop

--- backend/cmd/servo_test/main.go
+++ backend/cmd/servo_test/main.go
@@ -14,26 +14,21 @@ func main() {
 	}
 	defer rpio.Close()
 
-	// Pin 18 supports Hardware PWM
 	pin := rpio.Pin(18)
 	pin.Mode(rpio.Pwm)
-
-	// Set Frequency to 50Hz
-	// Formula: 19.2MHz / 64 / 6000 = 50Hz
 	pin.Freq(1_000_000)
 
-	// Move to 0 degrees (0.5ms)
-	pin.DutyCycle(500, 20_000)
-	fmt.Println("0 DEGREE")
-	time.Sleep(3 * time.Second)
+	for {
+		pin.DutyCycle(500, 20_000)
+		fmt.Println("0 DEGREE")
+		time.Sleep(2 * time.Second)
 
-	// Move to 90 degrees (1.5ms)
-	pin.DutyCycle(1500, 20_000)
-	fmt.Println("90 DEGREE")
-	time.Sleep(3 * time.Second)
+		pin.DutyCycle(1500, 20_000)
+		fmt.Println("90 DEGREE")
+		time.Sleep(2 * time.Second)
 
-	// Move to 180 degrees (2.5ms)
-	pin.DutyCycle(2500, 20_000)
-	fmt.Println("180 DEGREE")
-	time.Sleep(3 * time.Second)
+		pin.DutyCycle(2500, 20_000)
+		fmt.Println("180 DEGREE")
+		time.Sleep(2 * time.Second)
+	}
 }
